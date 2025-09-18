99230c247aa661d3d46744a2d3f955419144410f
gerardgoeller <gerard.goeller@gmail.com>
2025-09-18 15:12:42 +0200

    Update main.py

--- P2/P2C3/énoncé/main.py
+++ P2/P2C3/énoncé/main.py
@@ -1,1 +1,22 @@
 # Ecrivez votre code ici
+def salaire_mensuel(salaire_annuel):
+  resultat = float(salaire_annuel/12)
+  return resultat
+
+def salaire_hebdomadaire(salaire_mensuel):
+  resultat = float(salaire_mensuel / 4)
+  return resultat
+
+def salaire_horaire(salaire_hebdomadaire, heures_travaillees):
+  resultat = float(salaire_hebdomadaire / heures_travaillees )
+  return resultat
+
+montant_annuel = float(input("Entrez votre salaire annuel : "))
+heure_hebdo = float(input("Entrez nbre heures travaillees semaine : "))
+try:
+  mt_mois = salaire_mensuel(montant_annuel)
+  mt_hebdo = salaire_hebdomadaire(mt_mois)
+  mt_horaire = round(salaire_horaire(mt_mois,heure_hebdo))
+  print("Votre salaire horaire est de ",mt_horaire," euros")
+except ValueError:
+  print("Oops!  That was no valid number.  Try again...")
